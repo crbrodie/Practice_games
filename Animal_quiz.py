49a79bce19dd62b8104f3062c7632a1bf8fd93f9
crbrodie <38534995+crbrodie@users.noreply.github.com>
2018-06-01 19:16:31 -0500

    Update Animal_quiz.py

--- Animal_quiz.py
+++ Animal_quiz.py
@@ -1,28 +1,28 @@
 attempt = 0
 points = 0
-
-def check_answer(user_answer):
+user_answer = ''
+def check_answer(x):
     global points
     global attempt
-    global wtf
-    global player
+    global user_answer
+    
 
-    if user_answer.lower() == real_answer.lower():
+    if x.lower() == real_answer.lower():
         print(f'Good job! \nYou scored', 3 - int(attempt), 'points!')
         points = points + 3 - attempt
         attempt = 0
-        wtf=user_answer.lower()
+       
 
     else:
         attempt = attempt + 1
-        wtf=user_answer.lower()
 
-        if attempt == 3:
-            print('\nSorry, you are out of tries! \nThe correct answer was', real_answer, '\nYou ended with', points, 'points!')
-            player = 'idiot'
+
+        if attempt >= 3:
+            print('\nSorry, you are out of tries! \nThe correct answer was', real_answer, '\nYou have', points, 'points!')
+            user_answer = real_answer.lower()
             attempt = 0
 
-        if attempt < 3 and player != 'idiot':
+        else:
             print('Sorry, thats not right.  You have', 3-int(attempt), 'more live(s).')
         
 
@@ -30,31 +30,27 @@ def check_answer(user_answer):
 #Question 1
 print('Guess the Animal!')
 
-wtf = ''
-player = ''
 real_answer = 'Polar Bear'
-while wtf != real_answer.lower() and attempt < 3 and player!='idiot':
-    
-    check_answer(user_answer = input('\nWhich bear lives at the north pole?:  '))
+while user_answer != real_answer.lower() and attempt < 3:
+    user_answer = input('\nWhich bear lives at the north pole?:  ')
+    check_answer(user_answer)
 
 
 #Question 2
 print('\n\nQuestion 2: \n \n')
-wtf = ''
-player = ''
+
 real_answer = 'Cheetah'
-while wtf != real_answer.lower() and attempt < 3 and player!='idiot':
-    
-    check_answer(user_answer = input('Which is the fastest land animal?:  '))
+while user_answer != real_answer.lower() and attempt < 3:
+    user_answer = input('Which is the fastest land animal?:  ')
+    check_answer(user_answer)
 
 #Question 3
 
 print('\nQuestion 3: \n \n')
-wtf = ''
-player = ''
+
 real_answer = 'Blue Whale'
-while wtf != real_answer.lower() and attempt < 3 and player!='idiot':
-    
-    check_answer(user_answer = input('Which is the largest animal?:  '))
+while user_answer != real_answer.lower() and attempt < 3:
+    user_answer = input('Which is the largest animal?:  ')
+    check_answer(user_answer)
 
 print('Your final score is', str(points) + '!')
